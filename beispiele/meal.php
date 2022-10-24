e3363a8d72244065167dc75d7ee9fd181b38584f
ZulfahmiZaid <114953316+ZulfahmiZaid@users.noreply.github.com>
2022-10-24 15:11:09 +0200

    add erstesphp.php & meal.php

---
+++ beispiele/meal.php
@@ -0,0 +1,133 @@
+<?php
+const GET_PARAM_MIN_STARS = 'search_min_stars';
+const GET_PARAM_SEARCH_TEXT = 'search_text';
+const GET_PARAM_SHOW_DES = 'show_description';
+
+/**
+ * List of all allergens.
+ */
+$allergens = [
+    11 => 'Gluten',
+    12 => 'Krebstiere',
+    13 => 'Eier',
+    14 => 'Fisch', // es fehlt ein Komma
+    17 => 'Milch'
+];
+
+$meal = [
+    'name' => 'Süßkartoffeltaschen mit Frischkäse und Kräutern gefüllt',
+    'description' => 'Die Süßkartoffeln werden vorsichtig aufgeschnitten und der Frischkäse eingefüllt.',
+    'price_intern' => 2.90,
+    'price_extern' => 3.90,
+    'allergens' => [11, 13], // eckige Klammern
+    'amount' => 42             // Number of available meals
+];
+
+$ratings = [
+    [   'text' => 'Die Kartoffel ist einfach klasse. Nur die Fischstäbchen schmecken nach Käse. ',
+        'author' => 'Ute U.',
+        'stars' => 2 ],
+    [   'text' => 'Sehr gut. Immer wieder gerne',
+        'author' => 'Gustav G.',
+        'stars' => 4 ],
+    [   'text' => 'Der Klassiker für den Wochenstart. Frisch wie immer',
+        'author' => 'Renate R.',
+        'stars' => 4 ],
+    [   'text' => 'Kartoffel ist gut. Das Grüne ist mir suspekt.',
+        'author' => 'Marta M.',
+        'stars' => 3 ]
+];
+
+$showRatings = [];
+$searchedItem = "";
+
+if (!empty($_GET[GET_PARAM_SEARCH_TEXT])) { // es fehlt schließende Klammern
+    $searchTerm = strtolower($_GET[GET_PARAM_SEARCH_TEXT]);
+    foreach ($ratings as $rating) {
+        if (strtolower($rating['text']) == $searchTerm) { // strpos($rating['text'], $searchTerm) !== false
+            $showRatings[] = $rating;
+        }
+    }
+    $searchedItem = $searchTerm;
+} else if (!empty($_GET[GET_PARAM_MIN_STARS])) {
+    $minStars = $_GET[GET_PARAM_MIN_STARS];
+    foreach ($ratings as $rating) {
+        if ($rating['stars'] >= $minStars) {
+            $showRatings[] = $rating;
+        }
+    }
+} else {
+    $showRatings = $ratings;
+}
+
+function calcMeanStars(array $ratings) : float { // das Wort function
+    $sum = 0; // Beginn mit 0
+    foreach ($ratings as $rating) {
+        $sum += $rating['stars'] / count($ratings);
+    }
+    return $sum;
+}
+
+?>
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+        <meta charset="UTF-8"/>
+        <title>Gericht: <?php echo $meal['name']; ?></title>
+        <style>
+            * {
+                font-family: Arial, serif;
+            }
+            .rating {
+                color: darkgray;
+            }
+            td {
+                padding-right: 20px;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Gericht: <?php echo $meal['name']; ?></h1>
+        <p><?php
+            if($_GET[GET_PARAM_SHOW_DES] == 1){
+                echo $meal['description'];
+            }
+            ?>
+        </p>
+        <ul>
+            <?php
+            foreach ($allergens as $key => $value){
+                if($value != null){
+                    echo "<li>$value</li>";
+                }
+            }
+            ?>
+        </ul>
+        <h1>Bewertungen (Insgesamt: <?php echo calcMeanStars($ratings); ?>)</h1>
+        <form method="get">
+            <label for="search_text">Filter:</label>
+            <input id="search_text" type="text" name="search_text" value = "<?php echo $searchedItem ?>">
+            <input type="submit" value="Suchen">
+        </form>
+
+        <table class="rating">
+            <thead>
+            <tr>
+                <td>Text</td>
+                <td>Sterne</td>
+                <td>Author</td>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+        foreach ($showRatings as $rating) {
+            echo "<tr><td class='rating_text'>{$rating['text']}</td>
+                      <td class='rating_stars'>{$rating['stars']}</td>
+                      <td class='rating_author'>{$rating['author']}</td>
+                  </tr>";
+        }
+        ?>
+            </tbody>
+        </table>
+    </body>
+</html>
