5b0e3b0362656c3be4e4a643b8c184cd30284aeb
Hris <hrissdimov8@gmail.com>
2022-12-14 12:54:06 +0100

    a4 richtig

--- beispiele/m5_4.sql
+++ beispiele/m5_4.sql
@@ -6,10 +6,10 @@ WHERE name LIKE '%suppe%';
 
 CREATE VIEW IF NOT EXISTS  view_anmeldungen AS
 SELECT anzahlAnmeldungen FROM benutzer
-ORDER BY anzahlAnmeldungen ASC;
+ORDER BY anzahlAnmeldungen DESC;
 
 CREATE VIEW IF NOT EXISTS view_kategoriegerichte_vegetarisch AS
 SELECT GROUP_CONCAT(g.name) AS gerichtname, k.name AS kategoriename
-FROM gericht g RIGHT JOIN gericht_hat_kategorie ghk ON g.id = ghk.gericht_id
+FROM gericht g RIGHT JOIN gericht_hat_kategorie ghk ON g.id = ghk.gericht_id AND vegetarisch = 1
                RIGHT JOIN kategorie k ON ghk.kategorie_id = k.id
 GROUP BY k.name;
